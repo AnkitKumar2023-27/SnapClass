09d98036900052343f886dd58ce47ccead42d31d
Ankit Kumar <ankitk07306@gmail.com>
2026-07-31 19:35:15 +0530

    final touches

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import streamlit as st
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
 
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "snapclass-aith.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.write(f"**Subject:** {subject_name}")
